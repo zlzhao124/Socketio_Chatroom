a649630d09e87432d91744156241a9a9301bfddf
Helen Jiang <52161350+helenwhatsup@users.noreply.github.com>
2020-04-08 13:40:05 +0800

    Delete client.js

--- client.js
+++
@@ -1,9 +0,0 @@
-    var socketio = io.connect();
-
-    // get username
-    socketio.on("connect", function () {
-      var user = prompt("Enter the Username:");
-      socketio.emit("newUser", { username: user });
-    });
-
-
